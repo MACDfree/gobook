49ca5816a86f8c5ffb1b7df1e5d5ad89e33dac39
macd <macd@macddeMacBook-Air.local>
2017-05-09 23:08:24 +0800

    ch2

--- src/macd.me/ch2/tempconv/conv.go
+++ src/macd.me/ch2/tempconv/conv.go
@@ -1,11 +1,11 @@
 package tempconv
 
-//
+// CToF test
 func CToF(c Celsius) Fahrenheit {
 	return Fahrenheit(c*9/5 + 32)
 }
 
-//
+// FToC test
 func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
 }
--- src/macd.me/ch2/tempconv/tempconv.go
+++ src/macd.me/ch2/tempconv/tempconv.go
@@ -1,16 +1,23 @@
+// Package tempconv test package doc
 package tempconv
 
 import (
 	"fmt"
 )
 
-type Celsius float64    // 摄氏度
-type Fahrenheit float64 // 华氏度
+// Celsius 摄氏度
+type Celsius float64
+
+// Fahrenheit 华氏度
+type Fahrenheit float64
 
 const (
+	// AbsoluteZeroC test
 	AbsoluteZeroC Celsius = -273.15
-	FreezingC     Celsius = 0
-	BoilingC      Celsius = 100
+	// FreezingC test
+	FreezingC Celsius = 0
+	// BoilingC test
+	BoilingC Celsius = 100
 )
 
 func (c Celsius) String() string {
