e744eee59965fd158bb84093f80a0bb37fac66be
macd <macd@macddeMacBook-Air.local>
2017-05-13 23:20:40 +0800

    ch2&3

---
+++ src/macd.me/ch2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"macd.me/ch2/popcount"
+)
+
+func main() {
+	fmt.Println(popcount.PopCount(25555))
+	fmt.Println(popcount.PopCount25(25555))
+}
---
+++ src/macd.me/ch2/popcount/popcount.go
@@ -0,0 +1,30 @@
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount count bit 1
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+// PopCount25 count bit 1 of x
+func PopCount25(x uint64) int {
+	count := 0
+	for ; x != 0; x = x & (x - 1) {
+		count++
+	}
+	return count
+}
