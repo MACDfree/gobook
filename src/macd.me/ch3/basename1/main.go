05f28a2bdf3652fb248b4793d53a499f51026536
macd <macd@macddeMacBook-Air.local>
2017-05-17 23:09:03 +0800

    ch3

---
+++ src/macd.me/ch3/basename1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func basename(s string) string {
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		fmt.Printf("%s ", basename(arg))
+	}
+	fmt.Println()
+}
---
+++ src/macd.me/ch3/basename2/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		fmt.Printf("%s ", basename(arg))
+	}
+	fmt.Println()
+}
