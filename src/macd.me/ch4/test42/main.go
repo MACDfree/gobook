934ff0610b5953bac8c4e9d0c82e1e6576e3199f
MACDfree <hyitmacd@gmail.com>
2017-05-26 12:55:44 +0800

    ch4

--- src/macd.me/ch4/test42/main.go
+++ src/macd.me/ch4/test42/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
 	"fmt"
 	"os"
 )
 
+var t = flag.String("t", "256", "哈希算法类型，默认sha256（参数值为256），可选sha384（参数值为384）、sha512（参数值为512）")
+
 func main() {
-	for i, arg := range os.Args[1:] {
-		fmt.Printf("%d: %x\n", i, sha256.Sum256([]byte(arg)))
+	flag.Parse()
+	for _, arg := range flag.Args() {
+		switch *t {
+		case "256":
+			fmt.Printf("%s: %x\n", arg, sha256.Sum256([]byte(arg)))
+		case "384":
+			fmt.Printf("%s: %x\n", arg, sha512.Sum384([]byte(arg)))
+		case "512":
+			fmt.Printf("%s: %x\n", arg, sha512.Sum512([]byte(arg)))
+		default:
+			os.Exit(1)
+		}
 	}
 }
