6a58a6badb6b57bcd11089395e4bb560d287c213
macd <macd@macddeMacBook-Air.local>
2017-06-01 22:22:50 +0800

    ch4

---
+++ src/macd.me/ch4/append/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		z = x[:zlen]
+	} else {
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	z[len(x)] = y
+	return z
+}
+
+func appendInt1(x []int, y ...int) []int {
+	var z []int
+	zlen := len(x) + len(y)
+	if zlen <= cap(x) {
+		z = x[:zlen]
+	} else {
+		zcap := zlen
+		if zcap < 2*(len(x)+len(y)) {
+			zcap = 2 * (len(x) + len(y))
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	copy(z[len(x):], y)
+	return z
+}
+
+func main() {
+	z := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(cap(z))
+	z = appendInt1(z, 7, 8, 9)
+	fmt.Println(z)
+	fmt.Println(cap(z))
+}
